fdd91b81e58377aeabc730ddf2bd2d6bd7ebca9a
ankabutdev <Ankabut571@gmail.com>
2024-01-06 01:44:57 +0500

    add interface

---
+++ src/UserService/User.Infrastructure/Interfaces/Schedules/IScheduleRepository.cs
@@ -0,0 +1,7 @@
+﻿using UserService.Domain.Entities.Schedules;
+
+namespace UserService.Infrastructure.Interfaces.Schedules;
+
+public interface IScheduleRepository : IRepository<Schedule, Schedule>
+{
+}
