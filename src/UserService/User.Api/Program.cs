57e48fe889ea01441596470f8b4617eaf13f454d
ankabutdev <Ankabut571@gmail.com>
2024-01-06 01:43:25 +0500

    improve and added connection strings

--- src/UserService/User.Api/Program.cs
+++ src/UserService/User.Api/Program.cs
@@ -1,15 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using UserService.Application.Interfaces.Users;
+using UserService.Application.Services.Users;
+using UserService.Infrastructure.Data;
+using UserService.Infrastructure.Interfaces.Users;
+using UserService.Infrastructure.Repositories.Users;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSql"));
+});
+
+builder.Services.AddScoped<IUserService, UserServices>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +28,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
