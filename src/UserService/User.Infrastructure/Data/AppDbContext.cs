2bb4688cf3bb1d619a2a58d16a61a0d8cbbfe94f
ankabutdev <Ankabut571@gmail.com>
2024-01-06 01:44:38 +0500

    add dbContext

---
+++ src/UserService/User.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.Domain.Entities.Clients;
+using UserService.Domain.Entities.Companies;
+using UserService.Domain.Entities.Schedules;
+using UserService.Domain.Entities.Staffs;
+using UserService.Domain.Entities.Users;
+
+namespace UserService.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    { }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    public virtual DbSet<Client> Clients { get; set; }
+
+    public virtual DbSet<Company> Companies { get; set; }
+
+    public virtual DbSet<Schedule> Schedules { get; set; }
+
+    public virtual DbSet<Staff> Staffs { get; set; }
+
+}
