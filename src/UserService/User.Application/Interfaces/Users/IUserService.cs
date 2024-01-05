ad5f53e63324924d87a25a972079d21a2c39803f
ankabutdev <Ankabut571@gmail.com>
2024-01-06 01:43:44 +0500

    add User interface

---
+++ src/UserService/User.Application/Interfaces/Users/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using UserService.Application.DTOs.Users;
+using UserService.Domain.Entities.Users;
+
+namespace UserService.Application.Interfaces.Users;
+
+public interface IUserService
+{
+    public ValueTask<IEnumerable<User>> GetAllUserAsync();
+
+    public ValueTask<IEnumerable<User>> GetByIdAsync(Guid id);
+
+    public ValueTask<int> CountAsync();
+
+    public ValueTask<bool> CreateAsync(UserCreateDto dto);
+
+    public ValueTask<bool> UpdateAsync(Guid id, UserUpdateDto dto);
+
+    public ValueTask<bool> DeleteAsync(Guid id);
+}
