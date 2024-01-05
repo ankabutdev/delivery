e03b5f77268c60f56bd9ad9f837acd7c2b8d6584
ankabutdev <Ankabut571@gmail.com>
2024-01-06 01:45:04 +0500

    add interface

---
+++ src/UserService/User.Infrastructure/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace UserService.Infrastructure.Interfaces;
+
+public interface IRepository<TModel, TViewModel>
+{
+    public ValueTask<IEnumerable<TViewModel>> GetAllUserAsync();
+
+    public ValueTask<IEnumerable<TViewModel>> GetByIdAsync(Guid id);
+
+    public ValueTask<int> CountAsync();
+
+    public ValueTask<int> CreateAsync(TModel entity);
+
+    public ValueTask<int> UpdateAsync(TModel entity);
+
+    public ValueTask<int> DeleteAsync(Guid id);
+}
---
+++ src/UserService/User.Infrastructure/Interfaces/Users/IUserRepository.cs
@@ -0,0 +1,7 @@
+﻿using UserService.Domain.Entities.Users;
+
+namespace UserService.Infrastructure.Interfaces.Users;
+
+public interface IUserRepository : IRepository<User, User>
+{
+}
