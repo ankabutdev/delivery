ace3120e93b136e399250aa172b8b49e87ef4e78
ankabutdev <Ankabut571@gmail.com>
2024-01-06 01:43:33 +0500

    add dtos

---
+++ src/UserService/User.Application/DTOs/Users/UserCreateDto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using UserService.Domain.Enums;
+
+
+namespace UserService.Application.DTOs.Users;
+
+public class UserCreateDto
+{
+    public string FullName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
+
+    public IFormFile ImagePath { get; set; } = default!;
+
+    public UserRole UserRole { get; set; }
+
+    public Guid CompanyId { get; set; }
+
+    public Guid ClientId { get; set; }
+
+}
