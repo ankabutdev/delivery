05e773d550c61c0282bfa5209d6b12bbb6f5611e
ankabutdev <Ankabut571@gmail.com>
2024-01-06 01:42:57 +0500

    added Usercontroller

---
+++ src/UserService/User.Api/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserService.Application.DTOs.Users;
+using UserService.Application.Interfaces.Users;
+
+namespace UserService.Api.Controllers;
+
+[Route("api/users")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly IUserService _service;
+
+    public UsersController(IUserService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async ValueTask<IActionResult> GetAllAsync()
+    {
+        var result = await _service.GetAllUserAsync();
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async ValueTask<IActionResult> CreateAsync(UserCreateDto dto)
+    {
+        var result = await _service.CreateAsync(dto);
+        return Ok(result);
+    }
+}
